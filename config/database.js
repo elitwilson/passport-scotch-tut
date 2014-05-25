41a296aeff164323d4ed6bec3723819bdd37433f
elitwilson <elitwilson@gmail.com>
2014-05-24 20:27:50 -0400

    First Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+
+module.exports = {
+	// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://default:password@novus.modulusmongo.net:27017/xi2tyDys' 
+};
